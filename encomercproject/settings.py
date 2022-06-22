fd86866f4d7a6ea35133bb54182121882d2a65eb
Iljas <naruto7777-998@mail.ru>
2022-06-22 16:10:49 +0500

    encomercProject_encomdb

--- encomercproject/settings.py
+++ encomercproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +77,13 @@ WSGI_APPLICATION = 'encomercproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'encomdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Starcraft98@',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
