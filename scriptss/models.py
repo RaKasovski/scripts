c8d8d655c7de6ad78a231bba371c1dec0319a564
Iljas <naruto7777-998@mail.ru>
2022-06-28 15:47:41 +0500

    create_apps

--- scriptss/models.py
+++ scriptss/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import urllib.request
+import xml.dom.minidom
 
-# Create your models here.
+
+def get_data(xml_url):
+    web_file = urllib.request.urlopen(xml_url)
+    return web_file.read()
+
+
+def get_value(xml_content):
+    domtree = xml.dom.minidom.parse('value.xml')
+    value = domtree.documentElement
+    valuta = value.getElementsByTagName('Valute')
+    valut_dict = {}
+
+    for node in valuta:
+        for child in node.childNodes:
+            if child.nodeType == 1:
+                if child.tagName == 'Value':
+                    if child.firstChild.nodeType == 3:
+                        value = float(child.firstChild.data.replace(',', '.'))
+                if child.tagName == 'CharCode':
+                    if child.firstChild.nodeType == 3:
+                        char_code = child.firstChild.data
+        valut_dict[char_code] = value
+    return valut_dict
+
+
+def print_dict(dict):
+    for key in dict.keys():
+        if key == 'USD':
+            print(key, dict[key])
+            for key in dict.keys():
+                if key == 'EUR':
+                    print(key, dict[key])
+
+
+url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+data = get_value(get_data(url))
+print_dict(data)
--- valutShow/models.py
+++ valutShow/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.shortcuts import render
+from pprint import pprint
+import httplib2
+import googleapiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
 
-# Create your models here.
+CREDENTIALS_FILE = 'creds.json'
+
+spreadsheet_id = '1GCd4uUOanIGYHspKsT4Km4GJSQWm5oOL3Mgx_xRf7Cg'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = googleapiclient.discovery.build('sheets', 'v4', http=httpAuth)
+
+values = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheet_id,
+    range='A1:E10',
+    majorDimension='COLUMNS'
+).execute()
+pprint(values)
+
