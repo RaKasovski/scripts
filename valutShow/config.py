28bd4a803fb2bfc4958280f330be5833bf706432
Iljas <naruto7777-998@mail.ru>
2022-07-04 16:53:52 +0500

    all_works_add_db_table

---
+++ valutShow/config.py
@@ -0,0 +1,4 @@
+host = 'localhost'
+user = 'postgres'
+password = 'Starcraft98@'
+db_name = 'encomdb'
--- valutShow/views.py
+++ valutShow/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from typing import Union
 
-# Create your views here.
+import psycopg2
+from config import host, user, password, db_name
+
+connection = psycopg2.connect("")
+try:
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        pssword=password,
+        database=db_name
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+        print(f"Server version: {cursor.fetchnone()}")
+except Exception as ex:
+    print("[INFO] Error while working with PostgreSQL", ex)
+finally:
+    if  connection:
+        connection.close()
+        print("[INFO] PostgreSQL connection closed")
